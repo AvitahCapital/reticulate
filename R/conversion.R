cb43c2dd98e3a33eeeda30b8d5dde101405ee2cf
Joe <joe.robinson@avitahcapital.com>
2018-05-10 14:03:26 +0000

    better handling of datetimes

--- R/conversion.R
+++ R/conversion.R
@@ -252,6 +252,10 @@ py_to_r.pandas.core.frame.DataFrame <- function(x) {
     }
 
     #convert date columns
+    if('Timestamp' %in% class(converted[[i]][[1]]))
+      converted[[i]] <- as.POSIXct(py_to_r(x$`__getitem__`(column)$astype('str')), format="%Y-%m-%d %H:%M:%S")
+
+    #convert date columns
     if('datetime.date' %in% class(converted[[i]][[1]]))
       converted[[i]] <- as.Date(py_to_r(x$`__getitem__`(column)$astype('str')))
 
